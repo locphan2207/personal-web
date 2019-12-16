0dc1e0095aade6f8472f601c85cfc120b279d03d
Tan Loc Phan <tanloc.phan@smarkets.com>
2019-12-15 18:48:24 -0800

    Add video background, menu, and skills

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react"
+
+import Menu from "./Menu"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu />
+      <video
+        className="VideoBackground"
+        src={`${process.env.PUBLIC_URL}/beach.mp4`}
+        autoplay="true"
+        loop="true"
+        muted="true"
+        type="video/mp4"
+      />
+      <img
+        className="SkillCloud"
+        src={`${process.env.PUBLIC_URL}/skills.png`}
+        alt="skills"
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Menu.css"
+
+const Menu = props => {
+  return (
+    <div className="Menu">
+      <i id="menuIcon" class="fas fa-bars fa-2x"></i>
+    </div>
+  )
+}
+
+export default Menu
