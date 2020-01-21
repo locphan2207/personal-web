db0218150a953bcbcc99f062dbac13737de86a1d
Tan Loc Phan <tanloc.phan@smarkets.com>
2020-01-20 23:51:59 -0800

    Updates

---
+++ precache-manifest.0d439ca4d4c6b24133cbba263673f8c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84582b3343db691352d2812bd5255cb",
+    "url": "/tanloc-phan/index.html"
+  },
+  {
+    "revision": "16029906d68fe69e915b",
+    "url": "/tanloc-phan/static/css/main.07da3a47.chunk.css"
+  },
+  {
+    "revision": "9ebec2c55423b48fb5c8",
+    "url": "/tanloc-phan/static/js/2.5d7ab468.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tanloc-phan/static/js/2.5d7ab468.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16029906d68fe69e915b",
+    "url": "/tanloc-phan/static/js/main.9781502c.chunk.js"
+  },
+  {
+    "revision": "b96dae02a6122063d75c",
+    "url": "/tanloc-phan/static/js/runtime-main.f72c54e7.js"
+  },
+  {
+    "revision": "f49d9073fd6cc9cbb303f72b1103e385",
+    "url": "/tanloc-phan/static/media/avatar.f49d9073.jpg"
+  },
+  {
+    "revision": "b62e4fef2c2a3cc1b443b48bb1ab188c",
+    "url": "/tanloc-phan/static/media/down-arrow.b62e4fef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tanloc-phan/precache-manifest.0d439ca4d4c6b24133cbba263673f8c3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tanloc-phan/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
