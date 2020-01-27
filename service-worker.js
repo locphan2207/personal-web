2358ab3ea82aec63c9c69c347be17aaadafc0eaf
Tan Loc Phan <tanloc.phan@smarkets.com>
2020-01-27 01:02:38 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-web/precache-manifest.57846f1c2d8bfce80eb8714ddc1cb70d.js"
+  "/personal-web/precache-manifest.7b395838498b4fc8218859086a25389c.js"
 );
 
 self.addEventListener('message', (event) => {
