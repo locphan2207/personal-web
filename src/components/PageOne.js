ab280f08d876b025c6cada738ae2c3f0f05053f0
Tan Loc Phan <tanloc.phan@smarkets.com>
2020-02-02 12:55:29 -0800

    revise page 1

--- src/components/PageOne.js
+++ src/components/PageOne.js
@@ -1,17 +1,23 @@
-import React, { useState } from "react"
+import React from "react"
 import "./PageOne.css"
 import { ReactComponent as TopLeftSVG } from "../assets/top-left-svg.svg"
 import { ReactComponent as Illus } from "../assets/illu-right.svg"
 import { ReactComponent as BubbleRight } from "../assets/bubble-right.svg"
 import { ReactComponent as BubbleLeft } from "../assets/bubble-left.svg"
+import { ReactComponent as CornerLeaves } from "../assets/corner-leaves.svg"
 
 function PageOne() {
   return (
     <div className="page page-one">
-      <Illus />
-      <TopLeftSVG />
-      <BubbleLeft />
-      <BubbleRight />
+      <Illus className={"illu"} />
+      <TopLeftSVG className={"top-left-svg"} />
+      <BubbleLeft className={"bubble-left"} />
+      <BubbleRight className={"bubble-right"} />
+      <CornerLeaves className={"corner-leaves"} />
+      <div className="intro">
+        <h1>{"Tan Loc Phan"}</h1>
+        <h3>{"Software engineer"}</h3>
+      </div>
     </div>
   )
 }
