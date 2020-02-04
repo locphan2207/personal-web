07a09cdd56c4e47fd2b1324819b6ed4ed1897922
Tan Loc Phan <locphan2207@gmail.com>
2020-02-03 18:47:26 -0800

    Updates

--- precache-manifest.329979614d46dd1ae0e4f1524a50c074.js
+++ precache-manifest.329979614d46dd1ae0e4f1524a50c074.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bf1ea8b5fcae3ff310370853dad6a9fb",
+    "revision": "00b3b87ccebb7734bbddfa3b46d6e794",
     "url": "/personal-web/index.html"
   },
   {
-    "revision": "d949d4a382417b6cd9f2",
+    "revision": "f7bdb2815190dc2be0ee",
     "url": "/personal-web/static/css/main.a50efdcd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/personal-web/static/js/2.5d7ab468.chunk.js.LICENSE"
   },
   {
-    "revision": "d949d4a382417b6cd9f2",
-    "url": "/personal-web/static/js/main.086a9b51.chunk.js"
+    "revision": "f7bdb2815190dc2be0ee",
+    "url": "/personal-web/static/js/main.81c3a427.chunk.js"
   },
   {
     "revision": "5580ea9a64ec656b8535",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-web/precache-manifest.a793bd3c5632bc6a61fed449e9292037.js"
+  "/personal-web/precache-manifest.329979614d46dd1ae0e4f1524a50c074.js"
 );
 
 self.addEventListener('message', (event) => {
