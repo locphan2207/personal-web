cc764cff4a5f7b965f495ed6c69dcff603a2ce7c
Tan Loc Phan <locphan2207@gmail.com>
2020-02-08 01:11:16 -0800

    adjust shape

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from "react"
 import "./NavBar.css"
 
-function NavBar({ onWorkOpen }) {
+function NavBar({ onWorkOpen, onHomeOpen }) {
   return (
     <div className="nav-bar">
+      <h5 onClick={onHomeOpen}>{"HOME"}</h5>
       <h5 onClick={onWorkOpen}>{"WORK"}</h5>
       <h5>{"SKILLS"}</h5>
       <h5>{"ABOUT"}</h5>
