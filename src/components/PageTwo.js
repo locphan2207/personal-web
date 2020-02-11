39a921c371d06b48eda374773284e9f72b8004c7
Tan Loc Phan <tanloc.phan@smarkets.com>
2020-02-11 03:42:53 -0800

    page 2 right side finish

--- src/components/PageTwo.js
+++ src/components/PageTwo.js
@@ -93,58 +93,43 @@ function PageTwo({ isClosing, setClosingPage }) {
           </p>
         ))}
       </div>
-      <div className={"left"}>
-        {isDescriptionVisible && (
-          <div className={"desc-body"}>
-            <p className={"project-number"}>{`${selectedProjectIdx + 1 < 10 &&
-              "0"}${selectedProjectIdx + 1}`}</p>
-            <div className={"row project-body"}>
-              <div className={"project-desc-container"}>
-                <div className={"project-header"}>
-                  {PROJECTS[selectedProjectIdx].name
-                    .split("")
-                    .map((char, idx) => {
-                      if (char === " ")
-                        return <span key={idx}>&nbsp;&nbsp;</span>
-                      return (
-                        <span
-                          key={idx}
-                          className={"project-header-char"}
-                          style={{
-                            animationDelay: `${TITLE_LEFT_PANE_DELAY +
-                              idx * 0.03}s`,
-                          }}
-                        >
-                          {char}
-                        </span>
-                      )
-                    })}
+      {isDescriptionVisible && (
+        <div className={"desc-body"}>
+          <div className={"project-year-container"}>
+            {PROJECTS[selectedProjectIdx].years.map((year, idx) => (
+              <div key={idx}>
+                <div className={"year-wrapper"}>
+                  <p
+                    className={`project-year ${idx % 2 === 0 ? "even" : "odd"}`}
+                    key={year}
+                  >
+                    {year}
+                  </p>
                 </div>
-                <p className={"project-description"}>
-                  {PROJECTS[selectedProjectIdx].description}
-                </p>
+                {idx === 0 && <div className={"project-year-dash"} />}
               </div>
-              <div className={"project-year-container"}>
-                {PROJECTS[selectedProjectIdx].years.map((year, idx) => (
-                  <div key={idx}>
-                    <div className={"year-wrapper"}>
-                      <p
-                        className={`project-year ${
-                          idx % 2 === 0 ? "even" : "odd"
-                        }`}
-                        key={year}
-                      >
-                        {year}
-                      </p>
-                    </div>
-                    {idx === 0 && <div className={"project-year-dash"} />}
-                  </div>
-                ))}
-              </div>
-            </div>
+            ))}
           </div>
-        )}
-      </div>
+          <div className={"project-tags"}>
+            {PROJECTS[selectedProjectIdx].tags.split("").map((char, idx) => {
+              return (
+                <span
+                  key={idx}
+                  className={"project-tag-char"}
+                  style={{
+                    animationDelay: `${TITLE_LEFT_PANE_DELAY + idx * 0.03}s`,
+                  }}
+                >
+                  {char}
+                </span>
+              )
+            })}
+          </div>
+          <p className={"project-description"}>
+            {PROJECTS[selectedProjectIdx].description}
+          </p>
+        </div>
+      )}
     </div>
   )
 }
@@ -157,12 +142,7 @@ const PROJECTS = [
     years: ["2018", "2020"],
     description:
       "Smarkets is a betting exchange that offers the best odds with lowest commision. At Smarkets, I contributed directly to building a competitive online sportsbook application from its early stages to having thousands of users.",
-    bulletPoints: [
-      "Use React Native to build a cross-platform mobile application to deliver key features with top-notch animations for a perfect UI/UX.",
-      "Use Django to implement necessary database models and corresponding API endpoints for the app.",
-      "Setup a backend app using Flask for the geofence microservice.",
-      "Maintain the good performance by writing efficient functions and components. Write unit and automation tests on all platforms to avoid crashes. Apply fast bug fixing based on the crashlytic report.",
-    ],
+    tags: "Fullstack · Mobile dev",
   },
   {
     name: "Food Stories",
@@ -171,12 +151,7 @@ const PROJECTS = [
     years: ["2018"],
     description:
       "A CRUD full stack web to share cooking recipes, built with Ruby on Rails and React/Redux.",
-    bulletPoints: [
-      "Implemented user authentication using BCrypt to create login session without storing actual passwords.",
-      "Designed project as a single-page web app to only re-render components when making API calls to the backend.",
-      "Launched AWS as a cloud image storage and Heroku for web hosting.",
-      "Utilized JQuery and vanilla JavaScript to improve interactive UX/UI.",
-    ],
+    tags: "Fullstack · Mobile dev",
   },
   {
     name: "Data Block",
@@ -184,12 +159,7 @@ const PROJECTS = [
     link: "food-stories.com",
     years: ["2018"],
     description: "A data game built solely on Javascript with D3.js",
-    bulletPoints: [
-      "Utilized D3.js to create falling circles with sizes based on actual data numbers.",
-      "Applied Physics Laws to make collision, gravity, spring force and built methods for vector math and force calculation.",
-      "Implemented basic game knowledge with frame loop, position update, player control input, sound, instruction and score.",
-      "Designed a behavior for the in-game character to have it always move into the closest falling circle.",
-    ],
+    tags: "Fullstack · Mobile dev",
   },
   {
     name: "Emotion Diary",
@@ -198,11 +168,7 @@ const PROJECTS = [
     years: ["2018"],
     description:
       "A cross-platform mobile app which keeps track of users’ emotions everyday, built with React Native and Firebase.",
-    bulletPoints: [
-      "Implemented authentication and real-time database using Google’s Firebase cloud services.",
-      "Utilized React Native components to design a friendly and smooth UI/UX.",
-      "Implemented data visualization by creating interactive graphs with transitioning effects and touchable tooltips.",
-    ],
+    tags: "Fullstack · Mobile dev",
   },
 ]
 
