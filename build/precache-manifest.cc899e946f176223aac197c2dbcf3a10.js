7704f9f87fbbdef47713f2720853a6b04d127e1a
Tan Loc Phan <locphan2207@gmail.com>
2020-02-16 01:01:28 -0800

    Updates

---
+++ build/precache-manifest.cc899e946f176223aac197c2dbcf3a10.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ac9386d734d2bf4e425974113b188e",
+    "url": "/personal-web/index.html"
+  },
+  {
+    "revision": "623857fa3c6cf470510a",
+    "url": "/personal-web/static/css/main.ce65e090.chunk.css"
+  },
+  {
+    "revision": "de597ba9457916defba2",
+    "url": "/personal-web/static/js/2.f5cd2589.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/personal-web/static/js/2.f5cd2589.chunk.js.LICENSE"
+  },
+  {
+    "revision": "623857fa3c6cf470510a",
+    "url": "/personal-web/static/js/main.69239db7.chunk.js"
+  },
+  {
+    "revision": "5580ea9a64ec656b8535",
+    "url": "/personal-web/static/js/runtime-main.adc82d22.js"
+  },
+  {
+    "revision": "e36b085c818c8ab85f9eb768a4c933fd",
+    "url": "/personal-web/static/media/avatar.e36b085c.png"
+  },
+  {
+    "revision": "a2b46e1c88eb56e90496c928e410a42b",
+    "url": "/personal-web/static/media/data-block.a2b46e1c.png"
+  },
+  {
+    "revision": "b81e093ecae01e1384488f1913fce4ff",
+    "url": "/personal-web/static/media/emotion.b81e093e.png"
+  },
+  {
+    "revision": "ba701ce5fe57268f3685391d2558c18e",
+    "url": "/personal-web/static/media/food.ba701ce5.png"
+  },
+  {
+    "revision": "975972a205fd91a532d1b7433281af70",
+    "url": "/personal-web/static/media/palatino-linotype-bold.975972a2.ttf"
+  },
+  {
+    "revision": "96261bb90c9babbf8042ce7f43200d65",
+    "url": "/personal-web/static/media/palatino-linotype.96261bb9.ttf"
+  },
+  {
+    "revision": "e40e380ad1ac29cbb50c6cd5fc486939",
+    "url": "/personal-web/static/media/sbk.e40e380a.png"
+  },
+  {
+    "revision": "e13e40e3c809893a92c637dacc43d4dd",
+    "url": "/personal-web/static/media/triangle.e13e40e3.svg"
+  }
+]);
