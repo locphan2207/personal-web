a1c4e88cf8b3e0cc8e7a4904de036a8b5725ba38
Tan Loc Phan <locphan2207@gmail.com>
2020-04-06 19:30:23 -0700

    First commit version 2

---
+++ src/components/version2/App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./App.css"
+
+function App() {
+  return <p>HELLO</p>
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
-import App from "./components/App"
+// import App from "./components/version1/App"
+import App from "./components/version2/App"
 import * as serviceWorker from "./serviceWorker"
 
 setTimeout(() => {
   document.getElementById("loading-screen").style.display = "none"
   ReactDOM.render(<App />, document.getElementById("root"))
-}, 1000)
+}, 1500)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
