3dc0c8c0c6634f6e0575782bad3a63f2f8993125
Tan Loc Phan <locphan2207@gmail.com>
2020-04-06 20:10:17 -0700

    Greeting

--- src/components/version2/App.js
+++ src/components/version2/App.js
@@ -2,7 +2,16 @@ import React from "react"
 import "./App.css"
 
 function App() {
-  return <p>HELLO</p>
+  return (
+    <p className="greeting">
+      Hi there,
+      <br />
+      I'm a <span className="color-switch">software engineer</span> who loves
+      building <span className="underline">functional</span>,{" "}
+      <span className="underline">beautiful</span> and{" "}
+      <span className="underline">interactive</span> websites.
+    </p>
+  )
 }
 
 export default App
