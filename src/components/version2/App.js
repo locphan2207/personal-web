1fec07ee7c927966b55766c90527f7a2b78ca61c
Tan Loc Phan <locphan2207@gmail.com>
2020-04-08 05:26:53 -0700

    Couple adjustments

--- src/components/version2/App.js
+++ src/components/version2/App.js
@@ -73,7 +73,7 @@ function App() {
             <lottie-player
               src="https://assets1.lottiefiles.com/private_files/lf30_CkJZhd.json"
               background="transparent"
-              speed="0.5"
+              speed="0.3"
               loop
               // hover
               autoplay
@@ -106,37 +106,41 @@ function App() {
         </div>
       </div>
 
-      {/*Work*/}
-      <div className="big-section work">
-        <div className="work-section">
-          <p className="work-year">2018 - 2020</p>
-          <div className="work-info">
-            <h3>Smarkets</h3>
-            <p className="work-role">Software Engineer</p>
+      {/*Works and Projects*/}
+      <div className="big-section">
+        <div className="sub-section">
+          <p className="sub-section-title">WORK</p>
+          <div className="work-section">
+            <p className="work-year">2018 - 2020</p>
+            <div className="work-info">
+              <h3>Smarkets</h3>
+              <p className="work-role">Software Engineer</p>
+            </div>
+            <p className="work-tags">Fullstack · Mobile dev</p>
+            <Link className="work-link" text="More" />
+            <p className="work-desc">
+              <br />● Contributed directly to building a cross-platform mobile
+              application using React Native from the early stages to the app
+              store launch with the increase in user numbers from 0 to 9000
+              <br />● Constructed Redux Saga for API and websocket data flow
+              management to meet the requirements of every key feature
+              <br />● Integrated animated libraries and necessary styles to the
+              components to deliver the top-notch UI/UX from the mocked designs
+              <br />● Reduced the number of re-renderings and load times by
+              designing granular components with efficient time complexity
+              selectors
+              <br />● Designed a geofencing micro-service which enforces users’
+              locations with GPS and IP addresses using Flask in the backend and
+              React in the frontend
+              <br />● Implemented Django with Redis cache to build essential
+              database models and fast API endpoints for the social features
+              such as profiles, posts and comments
+              <br />● Applied unit and automation tests together with crashlytic
+              reports to avoid bugs and crashes
+            </p>
           </div>
-          <p className="work-tags">Fullstack · Mobile dev</p>
-          <Link className="work-link" text="More" />
-          <p className="work-desc">
-            <br />● Contributed directly to building a cross-platform mobile
-            application using React Native from the early stages to the app
-            store launch with the increase in user numbers from 0 to 9000
-            <br />● Constructed Redux Saga for API and websocket data flow
-            management to meet the requirements of every key feature
-            <br />● Integrated animated libraries and necessary styles to the
-            components to deliver the top-notch UI/UX from the mocked designs
-            <br />● Reduced the number of re-renderings and load times by
-            designing granular components with efficient time complexity
-            selectors
-            <br />● Designed a geofencing micro-service which enforces users’
-            locations with GPS and IP addresses using Flask in the backend and
-            React in the frontend
-            <br />● Implemented Django with Redis cache to build essential
-            database models and fast API endpoints for the social features such
-            as profiles, posts and comments
-            <br />● Applied unit and automation tests together with crashlytic
-            reports to avoid bugs and crashes
-          </p>
         </div>
+        <p className="sub-section-title">PROJECTS</p>
       </div>
     </>
   )
