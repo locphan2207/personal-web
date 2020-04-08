ae5d70bd3745d7f0143ea0f683a1333ff7d8d5f0
Tan Loc Phan <locphan2207@gmail.com>
2020-04-08 05:00:45 -0700

    Add work section

--- src/components/version2/App.js
+++ src/components/version2/App.js
@@ -5,6 +5,7 @@ import Link from "./Link"
 function App() {
   return (
     <>
+      {/*Intro*/}
       <div className="intro">
         <div className="name-greet">
           <h1 className="name">Tan Loc Phan</h1>
@@ -27,7 +28,9 @@ function App() {
           ></lottie-player>
         </div>
       </div>
-      <div className="expertise">
+
+      {/*Expertise*/}
+      <div className=" big-section expertise">
         <div className="expertise-section fronend">
           <h3>Frontend</h3>
           <p className="expertise-text">
@@ -49,6 +52,8 @@ function App() {
               background="transparent"
               speed="1"
               autoplay
+              // hover
+              // loop
             ></lottie-player>
           </div>
         </div>
@@ -68,8 +73,9 @@ function App() {
             <lottie-player
               src="https://assets1.lottiefiles.com/private_files/lf30_CkJZhd.json"
               background="transparent"
-              speed="0.1"
+              speed="0.5"
               loop
+              // hover
               autoplay
             ></lottie-player>
           </div>
@@ -78,10 +84,11 @@ function App() {
           <h3>CS Fundamental</h3>
           <p className="expertise-text">
             Applying computer science fundamental knowledge helps me building
-            good applications. Using the right algorithms and data structures
-            makes the code fast and clean. Every time I write a new function, I
-            make sure that the time and space complexity are on point. Knowing
-            different languages helps me adapt fast to different frameworks.
+            efficient applications. Using the right algorithms and data
+            structures makes the code fast and clean. Every time I write a new
+            function, I make sure that the time and space complexity are on
+            point. Knowing different languages helps me adapt fast to different
+            frameworks.
           </p>
           <p className="expertise-skills">
             <span>Skills</span>: JavaScript, Python, Ruby, C#
@@ -93,8 +100,42 @@ function App() {
               speed="1"
               loop
               autoplay
+              // hover
             ></lottie-player>
           </div>
+        </div>
+      </div>
+
+      {/*Work*/}
+      <div className="big-section work">
+        <div className="work-section">
+          <p className="work-year">2018 - 2020</p>
+          <div className="work-info">
+            <h3>Smarkets</h3>
+            <p className="work-role">Software Engineer</p>
+          </div>
+          <p className="work-tags">Fullstack · Mobile dev</p>
+          <Link className="work-link" text="More" />
+          <p className="work-desc">
+            <br />● Contributed directly to building a cross-platform mobile
+            application using React Native from the early stages to the app
+            store launch with the increase in user numbers from 0 to 9000
+            <br />● Constructed Redux Saga for API and websocket data flow
+            management to meet the requirements of every key feature
+            <br />● Integrated animated libraries and necessary styles to the
+            components to deliver the top-notch UI/UX from the mocked designs
+            <br />● Reduced the number of re-renderings and load times by
+            designing granular components with efficient time complexity
+            selectors
+            <br />● Designed a geofencing micro-service which enforces users’
+            locations with GPS and IP addresses using Flask in the backend and
+            React in the frontend
+            <br />● Implemented Django with Redis cache to build essential
+            database models and fast API endpoints for the social features such
+            as profiles, posts and comments
+            <br />● Applied unit and automation tests together with crashlytic
+            reports to avoid bugs and crashes
+          </p>
         </div>
       </div>
     </>
--- src/components/version2/Link.js
+++ src/components/version2/Link.js
@@ -1,9 +1,9 @@
 import React from "react"
 import "./Link.css"
 
-function Link({ text }) {
+function Link({ text, className = "" }) {
   return (
-    <div className="link-container">
+    <div className={`link-container ${className}`}>
       <p className="link-text">{text}</p>
       {/* <p>Connect</p> */}
       <div className="underline"></div>
