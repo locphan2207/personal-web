fe6958ef3332c9441dc7e444b615f7eaf03e8e60
Tan Loc Phan <locphan2207@gmail.com>
2020-04-09 04:55:52 -0700

    Header finished

--- src/components/version2/App.js
+++ src/components/version2/App.js
@@ -21,12 +21,16 @@ function App() {
   }))
 
   const transformName = props.progress.interpolate(progress => {
-    const dx = interpolateRange(progress, [0, 1], [0, -20])
+    const dx = interpolateRange(progress, [0, 1], [5, -25])
     const dy = interpolateRange(progress, [0, 1], [20, 0])
     const scale = interpolateRange(progress, [0, 1], [1, 0.5])
     return `translate(${dx}%, ${dy}vh) scale(${scale})`
   })
 
+  const backgroundHeader = props.progress.interpolate(
+    [0, 1],
+    ["#fcfcfcff", "#f5f5f5fa"]
+  )
   // Create scene after mounting
   useEffect(() => {
     createNameToHeaderScene(set)
@@ -34,12 +38,16 @@ function App() {
   return (
     <>
       {/*Intro*/}
-      <div className="sticky-header">
+      <animated.div
+        className="sticky-header"
+        style={{
+          backgroundColor: backgroundHeader,
+        }}
+      >
         <animated.h1 className="name" style={{ transform: transformName }}>
           Tan Loc Phan
         </animated.h1>
-        <animated.div className="sticky-header-bg"></animated.div>
-      </div>
+      </animated.div>
       <div className="intro">
         <div className="name-greet">
           <p className="greeting">
